d52552c345632f7004e5c2cd002a7e1fb25db4fb
Larissa <issaschmitz27@gmail.com>
2022-03-26 18:31:44 -0300

    Correção de envio

--- conf/conf.inc.php
+++ conf/conf.inc.php
@@ -3,7 +3,7 @@
     define('HOST', 'localhost');  
     define('DBNAME', 'aula');    
     define('USER', 'root');  
-    define('PASSWORD', '*Fada030648');
+    define('PASSWORD', '');
 
     define('DRIVER', 'mysql'); 
     define('CHARSET', 'utf8');
